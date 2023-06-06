d0544502346fd82d10961bfcb25b00716ce49c85
Durgun17 <batuhandurgun3@gmail.com>
2023-06-06 16:53:00 +0300

    Hepsi Patika

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react'
+import { FlatList, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native'
+
+
+function App () {
+  return(
+<View>
+  <Text>Mersdhaba</Text>
+</View>
+)}
+
+export default App
